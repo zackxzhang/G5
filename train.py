b7ae378cc252fd1b63b924cc50a8cb03f94ffc77
Zack Zhang <zack.zhang.coder@gmail.com>
2025-05-17 16:08:21 -0400

    rework training loop

--- train.py
+++ train.py
@@ -1,5 +1,7 @@
+import jax                                                        # type: ignore
+import time
 from pathlib import Path
-from g5.agent import Agent, ValueLearner, PolicyLearner
+from g5.agent import Learner, ValueLearner, PolicyLearner
 from g5.reward import Victory
 from g5.value import MLPValue
 from g5.policy import MLPPolicy
@@ -7,65 +9,88 @@ from g5.optim import CosineSchedule
 from g5.game import Replay, Simulator, Loader
 
 
-p1: Agent
-p2: Agent
+class Timer:
 
-n_stages = 10
-n_games = 100
-n_steps = n_stages * n_games * 100
+    def __init__(self, message: str):
+        self.message = message
+
+    def __enter__(self):
+        self.start = time.perf_counter()
+
+    def __exit__(self, *args):
+        self.end = time.perf_counter()
+        print('-' * 64)
+        print(self.message + ' takes time: ' + str(self.end - self.start))
+        print('-' * 64)
 
 
 if __name__ == '__main__':
 
+    n_stages = 20
+    n_games = 200
+    n_steps = n_stages * n_games * 150
+
+    p1: Learner
+    p2: Learner
+
     folder = Path('data/value/')
+    # p1 = ValueLearner.load(folder / 'p1.msgpack')
+    # p2 = ValueLearner.load(folder / 'p2.msgpack')
     p1 = ValueLearner(
         stone=+1,
         reward=Victory,
-        value=MLPValue(seed=21),
+        value=MLPValue(key=jax.random.key(21)),
         epsilon=CosineSchedule(n_steps, 0.8, 0.01),
-        seed=1,
+        key=jax.random.key(1),
     )
     p2 = ValueLearner(
         stone=-1,
         reward=Victory,
-        value=MLPValue(seed=43),
+        value=MLPValue(key=jax.random.key(43)),
         epsilon=CosineSchedule(n_steps, 0.8, 0.01),
-        seed=2,
+        key=jax.random.key(2),
     )
 
     # folder = Path('data/policy/')
+    # p1 = PolicyLearner.load(folder / 'p1.msgpack')
+    # p2 = PolicyLearner.load(folder / 'p2.msgpack')
     # p1 = PolicyLearner(
     #     stone=+1,
     #     reward=Victory,
-    #     value=MLPValue(seed=21),
-    #     policy=MLPPolicy(seed=87),
-    #     seed=1,
+    #     value=MLPValue(key=jax.random.key(21)),
+    #     policy=MLPPolicy(key=jax.random.key(87)),
+    #     key=jax.random.key(1),
     # )
     # p2 = PolicyLearner(
     #     stone=-1,
     #     reward=Victory,
-    #     value=MLPValue(seed=43),
-    #     policy=MLPPolicy(seed=65),
-    #     seed=2,
+    #     value=MLPValue(key=jax.random.key(43)),
+    #     policy=MLPPolicy(key=jax.random.key(65)),
+    #     key=jax.random.key(2),
     # )
 
-    for stage in range(0, n_stages):
+    with Timer(f"Training for {n_stages} stages ({n_games} games each)"):
 
-        simulator = Simulator(agents=(p1, p2), folder=folder)
-        replay_p1, replay_p2 = simulator(
-            stage=stage,
-            n_games=n_games,
-            save=True,
-        )
+        for stage in range(0, n_stages):
 
-        replay = Replay.load(folder / f'stage-{stage}_p1.npz')
-        loader = Loader(replay, batch_size=100)
-        for data in loader:
-            print([arr.shape for arr in data.values()])
-            p1.obs(**data)
+            simulator = Simulator(agents=(p1, p2), folder=folder)
+            replay_p1, replay_p2 = simulator(
+                stage=stage,
+                n_games=n_games,
+                save=True,
+            )
 
-        replay = Replay.load(folder / f'stage-{stage}_p2.npz')
-        loader = Loader(replay, batch_size=100)
-        for data in loader:
-            print([arr.shape for arr in data.values()])
-            p2.obs(**data)
+            replay = Replay.load(folder / f'stage-{stage}_p1.npz')
+            loader = Loader(replay, batch_size=100)
+            for data in loader:
+                print([arr.shape for arr in data.values()])
+                p1.obs(**data)
+
+            replay = Replay.load(folder / f'stage-{stage}_p2.npz')
+            loader = Loader(replay, batch_size=100)
+            for data in loader:
+                print([arr.shape for arr in data.values()])
+                p2.obs(**data)
+
+    p1.save(folder / 'p1.msgpack')
+    p2.save(folder / 'p2.msgpack')
